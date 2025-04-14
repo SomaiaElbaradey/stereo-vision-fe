b0e0acb02b085100f2000b35529fc823a5d1c3b1
somaiaelbaradey <somayaelbaradey@gmail.com>
2025-04-14 02:31:59 +0200

    chore: init react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CameraCapture from "./components/CameraCapture";
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center bg-primary">
+      <CameraCapture />
+    </div>
+  );
+}
+
+export default App;
