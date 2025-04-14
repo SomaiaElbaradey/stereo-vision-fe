dd4d31e16263fda6ca0cf056e58cd7cda4b3eeb8
somaiaelbaradey <somayaelbaradey@gmail.com>
2025-04-14 08:38:43 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "stereo-vision-fe",
+    base: "/stereo-vision-fe/",
     plugins: [react()],
 });
